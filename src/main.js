08bf2514742508e770dcb96d149bad230ca1aeb2
randomguy70 <onlychessfreeze0515@gmail.com>
2021-11-05 15:25:12 -0500

    updated

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 var config = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 1000,
+	height: 700,
 	backgroundColor: 0x000000,
 	
 	physics: {
 		default: 'arcade',
 		arcade: {
-				gravity: { y: 0, x: 0 },
-				debug: false
+			gravity: { y: 0, x: 0 },
+			debug: false
 		}
 	},
 	
 	scene: {
-		 preload: preload,
-		 create: create,
-		 update: update
+		preload: preload,
+		create: create,
+		update: update
 	}
 };
 
@@ -57,8 +57,8 @@ var aiConfig = {
 }
 
 var ballConfig = {
-	velocityX: 300,
-	velocityY: 300,
+	velocityX: 400,
+	velocityY: 400,
 	
 	startingX: (config.width / 2) - (30 / 2),
 	startingY: (config.height / 2) - (30 / 2),
@@ -68,13 +68,11 @@ function preload()
 {
 	this.load.image('ball', './assets/ball.png');
 	this.load.image('paddle', './assets/paddle.png');
+	this.load.image('horiz_border', './assets/horizontal_border.png');
 }
 
 function create()
 {
-	var r1 = this.add.rectangle(0, 0, 20, config.height, 0xC0C0C0).setOrigin(0, 0);
-	var r2 = this.add.rectangle(config.width - 20, 0, 20, config.height, 0xC0C0C0).setOrigin(0, 0);
-	
 	player = this.physics.add.sprite(playerConfig.startingX, playerConfig.startingY, 'paddle').setOrigin(0, 0).setImmovable();
 	ai = this.physics.add.sprite(aiConfig.startingX, aiConfig.startingY, 'paddle').setOrigin(0, 0).setImmovable();
 	ball = this.physics.add.sprite(ballConfig.startingX, ballConfig.startingY, 'ball').setOrigin(0, 0);
@@ -83,12 +81,11 @@ function create()
 	ai.setCollideWorldBounds(true);
 	ball.setCollideWorldBounds(true);
 	
-	ball.setVelocityX(ballConfig.velocityX);
-	ball.setVelocityY(ballConfig.velocityY);
-	ball.setBounce(1);
-	
 	this.physics.add.collider(player, ball);
-	this.physics.add.collider(ball, ai);
+	
+	this.physics.add.collider(ai, ball);
+	
+	initialiseBall();
 	
 	playerScoreText = this.add.text(config.width/3, config.height / 3, '0', { fontSize: '64px', fill: '#fff' });
 	aiScoreText = this.add.text(config.width*2/3, config.height*2 / 3, '0', { fontSize: '64px', fill: '#fff' });
@@ -100,6 +97,34 @@ function update()
 	updatePlayer();
 	updateAI();
 	checkForScore();
+}
+
+function initialiseBall() 
+{
+	ball.x = config.width / 2;
+	ball.y = Phaser.Math.Between(25, config.height - (20 + 5));
+	
+	ball.setBounce(1);
+	
+	var goingUp = Phaser.Math.Between(0, 1);
+	var goingRight = Phaser.Math.Between(0, 1);
+	
+	if(goingUp === 1)
+	{
+		ball.setVelocityY(ballConfig.velocityY);
+	}
+	else
+	{
+		ball.setVelocityY(- ballConfig.velocityY);
+	}
+	if(goingRight ===1)
+	{
+		ball.setVelocityX(ballConfig.velocityX);
+	}
+	else
+	{
+		ball.setVelocityX(- ballConfig.velocityX);
+	}
 }
 
 function updatePlayer()
@@ -138,25 +163,19 @@ function updateAI()
 
 function checkForScore()
 {
-	if(ball.x <= 20)
+	if(ball.x < 1)
 	{
 		aiScore++;
 		aiScoreText.setText(aiScore);
 		
-		ball.x = ballConfig.startingX;
-		ball.y = ballConfig.startingY;
-		ball.setVelocityX(-ballConfig.velocityX);
-		ball.setVelocityY(ballConfig.velocityY);
+		initialiseBall();
 	}
-	if(ball.x >= config.width - 60)
+	if(ball.x > (config.width - 41))
 	{
 		playerScore++;
 		playerScoreText.setText(playerScore);
 		
-		ball.x = ballConfig.startingX;
-		ball.y = ballConfig.startingY;
-		ball.setVelocityX(ballConfig.velocityX);
-		ball.setVelocityY(-ballConfig.velocityY);
+		initialiseBall();
 	}
 }
 
