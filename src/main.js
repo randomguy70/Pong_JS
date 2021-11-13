fe03e6dfa4d503848487d52e87cdfdfaa272f0da
randomguy70 <onlychessfreeze0515@gmail.com>
2021-11-12 21:33:51 -0600

    tried to fix import error

--- src/main.js
+++ src/main.js
@@ -1,20 +1,4 @@
-import { TitleScene } from "./scenes/titleScene.js";
-
-// class TitleScene extends Phaser.Scene {
-
-// 	constructor() {
-// 		super({key:'titleScene'});
-// 	}
-
-// 	preload() {
-// 		// this.load.image('background', 'images/background.jpg');
-// 	}
-
-// 	create() {
-		
-// 		var text = this.add.text(100,100, 'Welcome to my game');
-// 	}
-// }
+import TitleScene from "./scenes/titleScreen.js";
 
 var titleScene = new TitleScene();
 
