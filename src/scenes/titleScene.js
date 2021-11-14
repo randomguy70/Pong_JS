77f05496e62f33ebb85822185552f627fbd40541
randomguy70 <onlychessfreeze0515@gmail.com>
2021-11-13 20:10:43 -0600

    added button to my website

--- src/scenes/titleScene.js
+++ src/scenes/titleScene.js
@@ -10,38 +10,61 @@ const config =
 
 var playButton;
 var creditsButton;
-var gamesWebsiteUrl = 'https://randomguy70.github.io/games_website/';
 
 class TitleScene extends Phaser.Scene
 {
-
-	constructor() {
+	
+	constructor()
+	{
 		super({key:'titleScene'});
 	}
-
-	preload() {
+	
+	preload()
+	{
 		this.load.image('button', './assets/button.png');
 	}
-
-	create() {
+	
+	create()
+	{
 		
 		this.add.text(config.width / 2, config.height / 4, "Pong", {fontSize: 65, color: '#fff'}).setOrigin(0.5);
 		this.add.text(config.width / 2, config.height / 4 + 60, "cloned by Randomguy", {fontSize: 20, color: '#fff'}).setOrigin(.5);
-				
-		playButton = this.add.image(config.width / 2, config.height / 4 + 120, 'button');
 		
-		this.add.text(config.width / 2, config.height / 4 + 120, 'Play', {fontSize: 25}).setOrigin(0.5);
+		// play button
+		playButton = this.add.image(config.width / 2, config.height / 4 + 130, 'button');
 		
+		this.add.text(config.width / 2, config.height / 4 + 130, 'Play', {fontSize: 25}).setOrigin(0.5);
 		
 		playButton.setInteractive();
 		
-		playButton.on('pointerdown', () => {this.scene.start('gameScene');});
+		playButton.on('pointerdown', () => { this.scene.start('gameScene'); });
+		
+		// credits button
+		creditsButton = this.add.image(config.width / 2, config.height / 4 + 220, 'button');
+		
+		this.add.text(config.width / 2, config.height / 4 + 220, 'More Games', {fontSize: 25}).setOrigin(0.5);
+		
+		creditsButton.setInteractive();
+		
+		creditsButton.on('pointerdown', this.openExternalLink);
 	}
-}
+	
+	openExternalLink ()
+	{
+		
+   const url = 'https://randomguy70.github.io/games_website/';
 
-function playOver ()
-{
-	console.log('it worked!');
+   var s = window.open(url, '_blank');
+	
+   if (s && s.focus)
+   {
+      s.focus();
+   }
+   else if (!s)
+   {
+      window.location.href = url;
+   }
+}
 }
 
 export default TitleScene;
